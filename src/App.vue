d291cbef5c1870d34e9a3ffd1b6a3ffc0627e9d8
liuxuefei <liuxuefei@viewhigh.com>
2023-07-06 22:12:27 +0800

    feat: elemnt-plus

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="box">
     <h1>我是根硕</h1>
+    <div>
+      <el-button type="primary" :icon="Plus">主要模式</el-button>
+
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const str = '我爱你祖国'
+import {Plus} from "@element-plus/icons-vue"
+// const str = '我爱你祖国'
 </script>
 
 <style scoped lang="scss">
 .box {
   width: 400px;
   height: 400px;
-  background: black;
+  // background: black;
   h1 {
     color: white;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// 配置element的国际化
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//引入element-plus插件与样式
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
 
-const fun = () => {
-  console.log('123412我我我4')
-}
-fun()
-
-createApp(App).mount('#app')
+//获取应用实例对象
+const app = createApp(App)
+//安装element-plus插件
+app.use(ElementPlus, { locale: zhCn })
+//将应用挂在到挂载点上
+app.mount('#app')
